91dbdf27aae29bb5f3395124b4909915f71270c0
1Dore <oswaldo.aquino@galileo.edu>
2020-08-22 20:07:13 -0600

    component register y login
    componentes para paginar incial

--- Front-end/frontend-tipp/src/app/app.module.ts
+++ Front-end/frontend-tipp/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
