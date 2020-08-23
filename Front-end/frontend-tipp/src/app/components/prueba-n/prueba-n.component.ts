c78df26acfa9df4b4f6e4a58d1e5178c71e9f452
1Dore <oswaldo.aquino@galileo.edu>
2020-08-22 20:30:16 -0600

    otra prueba

---
+++ Front-end/frontend-tipp/src/app/components/prueba-n/prueba-n.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba-n',
+  templateUrl: './prueba-n.component.html',
+  styleUrls: ['./prueba-n.component.scss']
+})
+export class PruebaNComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
