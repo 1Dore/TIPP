875f48f9cfff50332d594eec41d28f31320af9f1
1Dore <oswaldo.aquino@galileo.edu>
2020-08-22 20:17:41 -0600

    prueba componente desde dore

--- Front-end/frontend-tipp/src/app/app.module.ts
+++ Front-end/frontend-tipp/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { PruebaComponent } from './components/prueba/prueba.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    PruebaComponent
   ],
   imports: [
     BrowserModule,
