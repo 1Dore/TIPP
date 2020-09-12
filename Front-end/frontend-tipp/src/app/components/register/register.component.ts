335739a6ba7c8806b56b866686c5ccedf2e8f1f4
1Dore <oswaldo.aquino@galileo.edu>
2020-09-11 22:01:26 -0600

    register sin probar

--- Front-end/frontend-tipp/src/app/components/register/register.component.ts
+++ Front-end/frontend-tipp/src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FormularioService } from 'src/app/services/formulario.service';
+import { RegisterForm } from './RegisterForm';
 
 @Component({
   selector: 'app-register',
@@ -17,13 +18,29 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
     this.formulario = this.fb.group({
       nombre:['', Validators.required],
-      Correo:['', Validators.required],
-      Password:['', Validators.required]
+      apellido:['', Validators.required],
+      correo:['', Validators.required],
+      password:['', Validators.required],
+
     })
   }
 
   onSubmit(){
-    
+    let formulario:RegisterForm = new RegisterForm();
+    formulario.nombre = this.formulario.value.nombre;
+    formulario.apellido = this.formulario.value.apellido;
+    formulario.correo = this.formulario.value.correo;
+    formulario.contraseña = this.formulario.value.password;
+    console.log(formulario.contraseña); 
+    this.formularioService.sentRegister(formulario).subscribe((data) => {
+      if(data.status == 1){
+        this.router.navigateByUrl('/TIPP_Register');
+      }
+      else {
+        alert("Ha habido un error, por favor, pruebe otra vez en unos minutos");
+      }
+    });
+    this.formulario.reset();
   }
 
   abrir(ruta:string){
