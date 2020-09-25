c1d4e01ccaeab1ef2447fb4e229ee1d73c4b018b
1Dore <oswaldo.aquino@galileo.edu>
2020-09-25 00:57:24 -0600

    creo admin, empiezo contratos

--- Back-end/src/app/routs/contratos.js
+++ Back-end/src/app/routs/contratos.js
@@ -4,7 +4,17 @@ const { response } = require('../../config/server');
 module.exports = (app) => {
 
     app.post('/newContrato', (req, res, next) => {
-        let querry =  `Insert into contratos (f_init, f_end, u_id, c_id) values (${req.body.inicio}, ${req.body.fin}, ${req.body.u_id}, ${req.body.c_id})`;
+        let querry =  `Insert into contratos (f_init, u_id, c_id) values (${req.body.inicio}, ${req.body.u_id}, ${req.body.c_id})`;
+        conn.query( querry, (error, formularios, cols) => {
+            if (error) res.json({status: 0, message: `${error}`});  
+            else res.json({status: 1, message: "Se creo un contrato satisfactoriamente", formularios});
+
+        });
+
+    });
+
+    app.post('/terminarContrato', (req, res, next) => {
+        let querry =  `Update into contratos (f_init, u_id, c_id) values (${req.body.inicio}, ${req.body.u_id}, ${req.body.c_id})`;
         conn.query( querry, (error, formularios, cols) => {
             if (error) res.json({status: 0, message: `${error}`});  
             else res.json({status: 1, message: "Se creo un contrato satisfactoriamente", formularios});
--- Front-end/frontend-tipp/src/app/app-routing.module.ts
+++ Front-end/frontend-tipp/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RegisterColaboradorComponent } from './components/register-colaborador/
 import { LogRegMenuComponent } from './components/log-reg-menu/log-reg-menu.component';
 import { ListaDeColaboradoresComponent } from './components/lista-de-colaboradores/lista-de-colaboradores.component';
 import { AdminLoginComponent } from './components/admin-login/admin-login.component';
+import { UsuarioContratoComponent } from './components/usuario-contrato/usuario-contrato.component';
 
 
 const routes: Routes = [
@@ -15,6 +16,7 @@ const routes: Routes = [
   {path: 'TIPP_Register', component: LogRegMenuComponent},
   {path: 'lista_de_colaboradores', component: ListaDeColaboradoresComponent},
   {path: 'admin-mode', component: AdminLoginComponent},
+  {path: 'contrato', component: UsuarioContratoComponent},
   {path: '**', redirectTo: "TIPP_Register", pathMatch:"full"}
 ];
 
