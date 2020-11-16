2a5dad5be9f6cee9952a7b7698725a17e330bc11
JoseAbril321 <46824246+JoseAbril321@users.noreply.github.com>
2020-11-15 22:08:35 -0600

    Update environment.ts

--- Front-end/frontend-tipp/src/environments/environment.ts
+++ Front-end/frontend-tipp/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   
-  apiURL: "http://localhost:3000/",
+  apiURL: "http://localhost:3030/",
 
   firebaseConfig : {
     apiKey: "AIzaSyAwhwLzRksa18UiEoeL1uaLGOdSlnaxDA8",
